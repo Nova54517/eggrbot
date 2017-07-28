8cb47f6d0b1c1be82779bea48b3144dbd27ad010
Nova54517 <Nova54517@gmail.com>
2017-07-27 21:46:55 -0400

    config file
    must change for functionality

---
+++ config.js
@@ -0,0 +1,14 @@
+// change token and prefix
+var default = {
+ "name":"eggrbot",
+ "desc":"a chatbot for discord",
+ "token":"bot token",
+ "prefix":"egg!"
+};
+
+var notdef = {
+ "name":"eggrbot",
+ "email":"bob@wtf.cc",
+ "password":"ultrasecurepassword112233",
+ "prefix":"egg!"
+}
